a1cc452f5e5b58f680a06f3acee1d991eee89074
macaos <macaoshu@gmail.com>
2020-04-22 01:40:41 +0900

    Make Grid Layout

---
+++ src/page/Main.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+
+const Main = () => {
+  return (
+    <div>
+      <div>Header</div>
+      <div className="cards">
+        <div className="card">ONE</div>
+        <div className="card">TWO</div>
+        <div className="card">THREE</div>
+        <div className="card">FOUR</div>
+        <div className="card">FIVE</div>
+        <div className="card">SIX</div>
+        <div className="card">SEVEN</div>
+        <div className="card">EIGHT</div>
+        <div className="card">NINE</div>
+        <div className="card">TEN</div>
+        <div className="card">ELEVEN</div>
+        <div className="card">TWELVE</div>
+        <div className="card">ONE</div>
+        <div className="card">TWO</div>
+        <div className="card">THREE</div>
+        <div className="card">FOUR</div>
+        <div className="card">FIVE</div>
+        <div className="card">SIX</div>
+        <div className="card">SEVEN</div>
+        <div className="card">EIGHT</div>
+        <div className="card">NINE</div>
+        <div className="card">TEN</div>
+        <div className="card">ELEVEN</div>
+        <div className="card">TWELVE</div>
+      </div>
+    </div>
+  );
+};
+
+export default Main;
