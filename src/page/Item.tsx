771da826da0324130ed54287dbf58fe4e480bdfb
macaos <help@kareui.com>
2020-04-26 00:18:31 +0900

    Item page code splitting

--- src/page/Item.tsx
+++ src/page/Item.tsx
@@ -1,26 +1,19 @@
-import React, { Component } from "react";
-import Card01 from "./items/card/Card01";
+import React, { Component, Suspense } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
-import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
-import LayoutEqualWidth from "./items/layout/LayoutEqualWidth";
-import Test from "./items/layout/Test";
 import Breadcrumb from "./Breadcrumb";
-import Block01 from "./items/block/Block01";
 
 class Item extends Component {
   render() {
+    const ItemPage = React.lazy(() => import(`.${window.location.pathname}`));
     return (
       <div className="Item">
         <Header />
         <Breadcrumb />
         <div className="content-area">
-          <BrowserRouter>
-            <Route path="/item/Card01" component={Card01} />
-            <Route path="/item/Block01" component={Block01} />
-            <Route path="/item/Test" component={Test} />
-            <Route path="/item/LayoutEqualWidth" component={LayoutEqualWidth} />
-          </BrowserRouter>
+          <Suspense fallback={<div>Loading...</div>}>
+            <ItemPage />
+          </Suspense>
         </div>
         <Footer />
       </div>
